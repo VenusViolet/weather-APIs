ed8e1581a11f3c3612e51844ccdc44004765740c
Venus Rodin <VenusViolet1234@gmail.com>
2023-01-03 20:08:41 -1000

    worked on funciton calling to set the default coordinates to Hilo, Hawaii

--- js/logic.js
+++ js/logic.js
@@ -3,3 +3,26 @@ const myurl = "https://api.openweathermap.org/data/2.5/";
 const forecast = "forecast?appid=";
 const weatherCall = "weather?appid=";
 const currentDate = moment().format('MM/DD?YYYY');
+
+let searchHistory = [];
+let fiveDayArray = [];
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, handle_error);
+
+    };
+    function handle_error(err) {
+        if(err.code == 1) {
+            userLat = 19.7216;
+            userLong = 155.0849;
+            $.ajax({
+                url: myurl + weatherCall + apiKey + "&lat=" + userLat.toFixed(1) + "&lon=" + userLong.toFixed(0),
+                method: "GET", 
+            }).then(function (r) {
+                searchACity(r.name);
+            });
+        };
+    };
+
+};
