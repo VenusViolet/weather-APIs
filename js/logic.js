3703515b271fb66454d55fd19346a21bcf11966a
Venus Rodin <VenusViolet1234@gmail.com>
2023-01-03 19:53:59 -1000

    imported api key

---
+++ js/logic.js
@@ -0,0 +1,5 @@
+const apiKey = "bdd4e019d62c00a0511377ab955c322a";
+const myurl = "https://api.openweathermap.org/data/2.5/";
+const forecast = "forecast?appid=";
+const weatherCall = "weather?appid=";
+const currentDate = moment().format('MM/DD?YYYY');
